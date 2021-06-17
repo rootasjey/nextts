03509def259921eb317b07444ccc7e1f848cf468
rootasjey <rootasjey@users.noreply.github.com>
2021-06-17 17:37:33 +0200

    feat(component): :sparkles: add [top-bar]

---
+++ components/top-bar.tsx
@@ -0,0 +1,56 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { Box, Typography, Paper, Button, makeStyles } from '@material-ui/core'
+import { UilRocket } from '@iconscout/react-unicons'
+import { css, jsx } from '@emotion/react'
+
+// A style sheet
+const useStyles = makeStyles({
+  root: {}, // a style rule
+  label: {}, // a nested style rule
+  button: {
+    marginRight: '10px',
+  },
+})
+
+// interface Props {
+//   styles: string
+// }
+
+export default function TopBar(): JSX.Element {
+  const classes = useStyles()
+  return (
+    <nav>
+      <Paper elevation={2} square>
+        <Box px={10} py={3}>
+          <div
+            css={css`
+              display: flex;
+              justify-content: space-between;
+            `}
+          >
+            <div
+              css={css`
+                display: flex;
+              `}
+            >
+              <Box component="span" pr={1}>
+                <UilRocket size="20" color="#4834d4" />
+              </Box>
+              <Typography component="span">
+                <b>Rocket Fire</b>
+              </Typography>
+            </div>
+
+            <div>
+              <Button className={classes.button}>Launches</Button>
+              <Button className={classes.button}>About</Button>
+              <Button className={classes.button}>GitHub</Button>
+            </div>
+            {/* <Typography component="span">Launches</Typography> */}
+          </div>
+        </Box>
+      </Paper>
+    </nav>
+  )
+}
